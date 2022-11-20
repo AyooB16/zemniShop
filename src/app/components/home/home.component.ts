c1b664bf3102050b1f6c1c7b07e74e9aef8f364f
oussem aloui <alouioussema697@gmail.com>
2022-11-20 20:11:27 +0100

    version6

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Produit } from 'src/app/produit';
+import { ProduitService } from 'src/app/produit.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  lesProduits!:Produit[];
+  @Input() id!:number;
+  @Input() title!:String;
+  @Input() category!:String;
+  @Input() price!:number;
+  @Input() image!:String;
+  @Input() disponibility!:boolean;
+  @Input() stars!:number;
+  product:any={};
+  constructor(private produitService:ProduitService) { }
+ 
   ngOnInit(): void {
+    this.produitService.getProduits().subscribe(data => this.lesProduits = data)
+  }
+  addToCart()
+  {
+      let ProductId=JSON.parse(localStorage.getItem('ProductId')||('0'))
+      this.product.id=ProductId;
+      let cart=JSON.parse(localStorage.getItem('cart')||('[]'))
+      cart.push(this.product);
+      localStorage.setItem('cart',JSON.stringify(cart));
+      localStorage.setItem('product',JSON.stringify(ProductId+1))
   }
 
 }
