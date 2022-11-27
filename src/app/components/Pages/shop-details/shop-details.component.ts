76a148d91cee95f3e812f488047c082fea669df9
oussem aloui <alouioussema697@gmail.com>
2022-11-27 20:48:49 +0100

    v12

--- src/app/components/Pages/shop-details/shop-details.component.ts
+++ src/app/components/Pages/shop-details/shop-details.component.ts
@@ -30,6 +30,7 @@ export class ShopDetailsComponent implements OnInit {
     }
     
   }
+  
 }
 
 
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,21 +11,13 @@ import { ProduitService } from 'src/app/produit.service';
 export class HomeComponent implements OnInit {
   lesProduits!:Produit[];
   id!:number;
-  product:any={};
+
   constructor(private produitService:ProduitService,private route:Router) { }
  
   ngOnInit(): void {
     this.produitService.getProduits().subscribe(data => this.lesProduits = data)
   }
-  addToCart()
-  {
-      let ProductId=JSON.parse(localStorage.getItem('ProductId')||('0'))
-      this.product.id=ProductId;
-      let cart=JSON.parse(localStorage.getItem('cart')||('[]'))
-      cart.push(this.product);
-      localStorage.setItem('cart',JSON.stringify(cart));
-      localStorage.setItem('product',JSON.stringify(ProductId+1))
-  }
+
 
 
 }
